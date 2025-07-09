39cf29ccfa4412936eda55c632a348d829b68d3d
milky314 <elesbee913@gmail.com>
2025-07-10 02:07:11 +1000

    Update meme_bot.py

--- meme_bot.py
+++ meme_bot.py
@@ -3,7 +3,16 @@ import requests
 import tweepy
 
 def post_meme():
-    # Auth setup for Twitter API v2 using tweepy.Client
+    # Step 1: Auth for v1.1 (media upload)
+    auth = tweepy.OAuth1UserHandler(
+        os.getenv("API_KEY"),
+        os.getenv("API_SECRET"),
+        os.getenv("ACCESS_TOKEN"),
+        os.getenv("ACCESS_TOKEN_SECRET")
+    )
+    api_v1 = tweepy.API(auth)
+
+    # Step 2: Auth for v2 (tweet posting)
     client = tweepy.Client(
         consumer_key=os.getenv("API_KEY"),
         consumer_secret=os.getenv("API_SECRET"),
@@ -11,20 +20,20 @@ def post_meme():
         access_token_secret=os.getenv("ACCESS_TOKEN_SECRET")
     )
 
-    # Meme details
+    # Meme info
     image_url = "https://i.redd.it/3vqlgh6mxqb91.jpg"
     caption = "Elon trying to regulate AI after teaching it how to ratio people"
     filename = "meme.jpg"
 
-    # Download meme image
+    # Download image
     response = requests.get(image_url)
     with open(filename, "wb") as f:
         f.write(response.content)
 
-    # Upload image to Twitter
-    media = client.upload_media(filename=filename)
+    # Upload image (v1.1)
+    media = api_v1.media_upload(filename)
 
-    # Post tweet with image
+    # Post tweet (v2)
     client.create_tweet(text=caption, media_ids=[media.media_id])
 
     os.remove(filename)
