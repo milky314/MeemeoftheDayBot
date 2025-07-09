c4f09c28583b0c10f67f73de18c75638ee2e31bc
milky314 <elesbee913@gmail.com>
2025-07-10 02:05:52 +1000

    Update meme_bot.py

--- meme_bot.py
+++ meme_bot.py
@@ -2,27 +2,32 @@ import os
 import requests
 import tweepy
 
-def post_meme(api):
+def post_meme():
+    # Auth setup for Twitter API v2 using tweepy.Client
+    client = tweepy.Client(
+        consumer_key=os.getenv("API_KEY"),
+        consumer_secret=os.getenv("API_SECRET"),
+        access_token=os.getenv("ACCESS_TOKEN"),
+        access_token_secret=os.getenv("ACCESS_TOKEN_SECRET")
+    )
+
+    # Meme details
     image_url = "https://i.redd.it/3vqlgh6mxqb91.jpg"
     caption = "Elon trying to regulate AI after teaching it how to ratio people"
     filename = "meme.jpg"
 
+    # Download meme image
     response = requests.get(image_url)
-    with open(filename, "wb") as img:
-        img.write(response.content)
+    with open(filename, "wb") as f:
+        f.write(response.content)
 
-    api.update_status_with_media(caption, filename)
+    # Upload image to Twitter
+    media = client.upload_media(filename=filename)
+
+    # Post tweet with image
+    client.create_tweet(text=caption, media_ids=[media.media_id])
+
     os.remove(filename)
 
-def main():
-    auth = tweepy.OAuth1UserHandler(
-        os.getenv("API_KEY"),
-        os.getenv("API_SECRET"),
-        os.getenv("ACCESS_TOKEN"),
-        os.getenv("ACCESS_TOKEN_SECRET")
-    )
-    api = tweepy.API(auth)
-    post_meme(api)
-
 if __name__ == "__main__":
-    main()
+    post_meme()
